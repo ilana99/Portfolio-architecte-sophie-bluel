c780dd2761a5de89f877129828daf6ff0ebf14d2
ilana99 <132579012+ilana99@users.noreply.github.com>
2023-05-10 16:59:20 +0200

    Update script.js

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,46 +1,64 @@
-var jsonData;
-
 async function getWorks() {
     let response = await fetch("http://localhost:5678/api/works");
-    var jsonData = await response.json();
+    let jsonData = await response.json();
     console.log(jsonData);
+
+    const gallery = document.getElementById("gallery");
+    function showWorks() {
+       
+        for (let i = 0; i < jsonData.length; i++) {
+            const figure = document.createElement("figure");
+            gallery.appendChild(figure);
+            const img = document.createElement("img");
+            img.src = jsonData[i].imageUrl;
+            const figcaption = document.createElement("figcaption");
+            figcaption.innerHTML = jsonData[i].title;
+            figure.appendChild(img);
+            figure.appendChild(figcaption);
+        }
+    }
+
+    showWorks();
+    
+    const objetsBouton = document.getElementById("objets");
+    const appartementsBouton = document.getElementById("appartements");
+    const hotelsBouton = document.getElementById("hotels");
+    const tousBouton = document.getElementById("tous");
+
+    function displayImagesParCategory(categoryName) {
+        const imagesParCategory = jsonData.filter(item => item.category.name === categoryName);
+        gallery.innerHTML = "";
+
+        for (let j = 0; j < imagesParCategory.length; j++) {
+            const figure = document.createElement("figure");
+            gallery.appendChild(figure);
+            const img = document.createElement("img");
+            img.src = imagesParCategory[j].imageUrl;
+            const figcaption = document.createElement("figcaption");
+            figcaption.innerHTML = imagesParCategory[j].title;
+            figure.appendChild(img);
+            figure.appendChild(figcaption);
+        }
+    }
+
+    objetsBouton.addEventListener("click", function () {
+        displayImagesParCategory("Objets");
+    });
+
+    appartementsBouton.addEventListener("click", function () {
+        displayImagesParCategory("Appartements");
+    });
+
+    hotelsBouton.addEventListener("click", function () {
+        displayImagesParCategory("Hotels & restaurants");
+    });
+
+    tousBouton.addEventListener("click", function () {
+        gallery.innerHTML = "";
+        showWorks();
+    });
+
 }
 
 getWorks();
 
-function afficherWorks() {
-    const gallery = document.getElementById("gallery");
-    for (let i = 0; i < 11; i++) {
-        const figure = document.createElement("figure");
-        gallery.appendChild(figure);
-        const img = document.createElement("img");
-        img.src = jsonData[i].imageUrl;
-        const figcaption = document.createElement("figcaption");
-        figcaption.innerHTML = jsonData[i].title;
-        figure.appendChild(img);
-        figure.appendChild(figcaption);
-    }
-}
-
-afficherWorks();
-
-
-const buttons = document.querySelector("button");
-
-const tousBouton = document.getElementById("tous");
-const objetsBouton = document.getElementById("objets");
-const appartementsBouton = document.getElementById("appartements");
-const hotelsBouton = document.getElementById("hotels");
-
-tousBouton.addEventListener("click", function () {
-
-});
-objetsBouton.addEventListener("click", function () {
-
-});
-appartementsBouton.addEventListener("click", function () {
-
-});
-hotelsBouton.addEventListener("click", function () {
-
-});
